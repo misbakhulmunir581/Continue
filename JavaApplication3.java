5761f7889ff835b3a279b0891732d2076f246060
misbakhulmunir581 <56871810+misbakhulmunir581@users.noreply.github.com>
2019-10-30 20:48:37 +0700

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int nilai = 90; nilai > 80; nilai --) {
+      if (nilai == 83) {
+          continue;
+      } 
+       System.out.println(nilai) ;
+        }
+        }
+
+    }
+    
+    
